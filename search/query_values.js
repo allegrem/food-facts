8e50ba918f5096cc3418f7ab206a293ce0c7ad30
amatissart <adrien.matissart@live.fr>
2014-02-09 14:33:40 +0100

    fix max calculation

--- search/query_values.js
+++ search/query_values.js
@@ -50,7 +50,7 @@ function values_from_tag(selectedItem) {
 		}
 		else if(option == 'max'){
 			for(var i=0;i<nprop;i++){
-				var temp = 0;
+				var temp = Number.MIN_VALUE;
 				for( var j=0;j<products_subset.length;j++){
 					if (+products_subset[j][properties[i]]>temp){
 						temp = +products_subset[j][properties[i]];
